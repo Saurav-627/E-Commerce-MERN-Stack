64c7fccec5ebb3428c69b1b7e956850200d612fa
Saurav Luitel <117023267+Saurav-627@users.noreply.github.com>
2025-06-29 18:29:57 +0545

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL
+// const API_BASE_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://e-commerce-mern-stack-o7j7.onrender.com",
   timeout: 10000,
 });
 
