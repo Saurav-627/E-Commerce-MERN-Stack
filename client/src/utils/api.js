05d291a97f6b3619da22f56e17787653d97a5199
saurav <saurav@pocketpandit.com>
2025-07-08 21:29:44 +0545

    updated: API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,8 @@ import toast from 'react-hot-toast';
 // const API_BASE_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-  baseURL: "https://e-commerce-mern-stack-o7j7.onrender.com",
+  // baseURL: "https://e-commerce-mern-stack-o7j7.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
   timeout: 10000,
 });
 
